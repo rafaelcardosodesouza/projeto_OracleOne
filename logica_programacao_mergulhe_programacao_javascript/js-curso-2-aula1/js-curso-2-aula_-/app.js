7c5cf8162cda47f2391b64ede251588b31c7ef69
Sr. Rafael Cardoso <rafaelcsadm@hotmail.com>
2024-01-25 13:54:08 -0300

    create first challenge

---
+++ logica_programacao_mergulhe_programacao_javascript/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p')
+
+titulo.innerHTML = 'Jogo do numero secreto';
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function verificarChute(){
+    console.log("O botão foi clicado");
+}
